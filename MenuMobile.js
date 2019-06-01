ff457183b32f58d9ef0bf465061cbf8c32db0841
inversia <mrs.shpritz@gmail.com>
2019-06-01 07:16:24 +0300

    menu files add

---
+++ MenuMobile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MenuMobile = () =>
+    <div className="menu mobile">
+        <a href="#menu" className="hamburger-menu link"></a>
+        <a href="#basket" className="meat link"></a>
+        <a href="#basket" className="broccoli link"></a> 
+        <a href="#basket" className="fish link"></a> 
+        <a href="#basket" className="mix link"></a> 
+        <a href="#basket" className="basket link"></a> 
+    </div>
+
+export default MenuMobile
