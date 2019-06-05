5c27a542004647e58fe6d5e32ada8f7fe816ab1d
inversia <mrs.shpritz@gmail.com>
2019-06-06 02:32:49 +0300

    toggle background add

--- MenuMobile.js
+++ MenuMobile.js
@@ -3,7 +3,7 @@ import React from 'react'
 const MenuMobile = ({onSelect }) =>
     <div className="menu mobile">
         <a href="#menu" className="hamburger-menu link"></a>
-        <a href="#meatMobile" className="meat link" onClick={() => onSelect ('veg') }></a>
+        <a href="#meatMobile" className="meat link" onClick={() => onSelect ('meat') }></a>
         <a href="#vegMobile" className="broccoli link" onClick={() => onSelect ('veg') }></a> 
         <a href="#fishMobile" className="fish link" onClick={() => onSelect ('fish') }></a> 
         <a href="#mixMobile" className="mix link" onClick={() => onSelect ('mix') }></a> 
