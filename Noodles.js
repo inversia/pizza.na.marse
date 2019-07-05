bc2756206f4efd3ce4fb252f17391674cee84503
inversia <mrs.shpritz@gmail.com>
2019-07-05 03:01:22 +0300

    pasta section hex add

--- Noodles.js
+++ Noodles.js
@@ -4,20 +4,13 @@ import './Noodles.css'
 const Noodles = () => 
     
     <div className='noodles-wrapper'>
-        <h1>Космическая лапша</h1>
-        <div className='noodles'>
-
-        <svg viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
-        <defs>
-            <pattern id="img" patternUnits="userSpaceOnUse" width="100" height="100">
-            <image xlinkHref="https://farm4.staticflickr.com/3165/5733278274_2626612c70.jpg" x="-25" width="150" height="100" />
-            </pattern>
-        </defs>
-        <polygon points="50 1 95 25 95 75 50 99 5 75 5 25" fill="url(#img)"/>
-        </svg>
-
         
-
+        <div className='noodles'>
+            <h1>Космическая лапша</h1>
+            <div className='pasta-item' style={{height:"25vw", width:"25vw", left:"10vw", top:"5vw"}}>
+                <div className='background'></div>
+                <div className='foreground' style={{backgroundImage:"url(/art/salad5.jpg)"}}></div>
+            </div>
         </div>
     </div>
 
