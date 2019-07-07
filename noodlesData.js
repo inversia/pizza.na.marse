ce2c516de15100d24bf6ac7a23c6e36eed5f606b
inversia <mrs.shpritz@gmail.com>
2019-07-07 06:06:36 +0300

    data and media add

---
+++ noodlesData.js
@@ -0,0 +1,103 @@
+const noodlesData = [
+    {
+        name: 'лапша1',
+        composition: ['cливочный соус', 'чеддер', 'пармезан', 'моцарелла', 'горгонзола'],
+        price: [500],
+        height:"23vw",
+        width:"23vw", 
+        left:"67vw", 
+        top:"7vw", 
+        animationDelay:"1.5s",
+        backgroundImage:"url(/art/salad2.jpg)"
+    },
+
+    {
+        name: 'лапша2',
+        composition: ['cливочный соус', 'чеддер', 'пармезан', 'моцарелла', 'горгонзола'],
+        price: [500],
+        height:"23vw", 
+        width:"23vw", 
+        left:"31.8vw", 
+        top:"27.5vw", 
+        animationDelay:"0.5s",
+        backgroundImage:"url(/art/salad2.jpg)"
+    },
+
+    {
+        name: 'лапша3',
+        composition: ['cливочный соус', 'чеддер', 'пармезан', 'моцарелла', 'горгонзола'],
+        price: [500],
+        height:"19vw", 
+        width:"19vw", 
+        left:"69vw", 
+        top:"30vw", 
+        animationDelay:"2.3s",
+        backgroundImage:"url(/art/salad2.jpg)",
+    },
+
+    {
+        name: 'лапша4',
+        composition: ['cливочный соус', 'чеддер', 'пармезан', 'моцарелла', 'горгонзола'],
+        price: [500],
+        height:"18vw", 
+        width:"18vw", 
+        left:"53vw", 
+        top:"21.5vw", 
+        animationDelay:"0.85s",
+        backgroundImage:"url(/art/salad2.jpg)",
+    },
+
+    {
+        name: 'лапша5',
+        composition: ['cливочный соус', 'чеддер', 'пармезан', 'моцарелла', 'горгонзола'],
+        price: [500],
+        height:"18vw", 
+        width:"18vw", 
+        left:"16vw", 
+        top:"21vw", 
+        animationDelay:"1.9s",
+        backgroundImage:"url(/art/salad2.jpg)",
+    },
+
+    {
+        name: 'лапша6',
+        composition: ['cливочный соус', 'чеддер', 'пармезан', 'моцарелла', 'горгонзола'],
+        price: [500],
+        height:"18vw", 
+        width:"18vw", 
+        left:"16vw", 
+        top:"39vw", 
+        animationDelay:"5.2s",
+        backgroundImage:"url(/art/salad4.jpg)",
+    },
+
+    {
+        name: 'hex1',
+        height:"8vw", 
+        width:"8vw", 
+        left:"11vw", 
+        top:"17.5vw", 
+        animationDelay:"3.6s",
+        backgroundColor:"orange",
+    },
+    
+    {
+        name: 'hex2',
+        height:"9vw", 
+        width:"9vw", 
+        left:"57.5vw", 
+        top:"44vw",
+        backgroundColor:"ffc400",
+    },
+
+    {
+        name: 'hex3',
+        height:"9vw", 
+        width:"9vw", 
+        left:"65.8vw", 
+        top:"49vw",
+        backgroundColor:"ff9100",
+    }, 
+]
+
+export default noodlesData
