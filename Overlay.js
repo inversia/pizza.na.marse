f7f9e80709224fb4230f19e80c7e1a2abea3b7d1
inversia <mrs.shpritz@gmail.com>
2019-08-07 10:53:45 +0300

    overlay menu refactoring & info-panel fix

--- Overlay.js
+++ Overlay.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import pizzaData from './pizzaData'
 import noodlesData from './noodlesData'
 import PizzaInfo from './PizzaInfo'
@@ -13,6 +13,7 @@ export default function Overlay ({ setOverlayVisible, activeProduct, setActivePr
 
     const productListEl   = useRef ()
     const productListSize = useComponentSize (productListEl)
+    const [dropdownMenuVisible, setDropdownMenuVisible] = useState(false)
 
     const products = {
         pizzas:  pizzaData,
@@ -20,14 +21,17 @@ export default function Overlay ({ setOverlayVisible, activeProduct, setActivePr
         salads:  saladsData,
     }
 
+    const productLabels = { pizzas: 'ПИЦЦЫ', noodles: 'ЛАПША', salads: 'САЛАТЫ' }
+
     return <div className='product-overlay'>
                 <div className='overlay-menu'>
                     <div className='back' onClick={() => setOverlayVisible (false)} >⇐</div>
-                    <div className='choose-type'>
-                        <div className='' onClick={() => setActiveProduct('pizzas')}>ПИЦЦА</div>
-                        <div className='' onClick={() => setActiveProduct('noodles')}>ЛАПША</div>
-                        <div className='' onClick={() => setActiveProduct('salads')}>САЛАТЫ</div>
-                    </div>
+                    
+                    {dropdownMenuVisible
+                        ? <div className='choose-type' onClick={() => dropdownMenuVisible ? setDropdownMenuVisible(false) : setDropdownMenuVisible(true)}>
+                              {Object.entries(productLabels).map(([k, v]) => <div onClick={() => setActiveProduct(k)}>{v}</div>)}
+                          </div>
+                        : <div className='' onClick={() => setDropdownMenuVisible(true)}>{productLabels[activeProduct]}</div>}
                     <div className='choose-taste'>Выбрать тип начинки</div>
                     <div className='bucket'>Корзина</div>
                 </div>
