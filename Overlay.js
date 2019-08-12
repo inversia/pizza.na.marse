5948d15182891c49db0838be8b6d8d661a7fe38e
inversia <mrs.shpritz@gmail.com>
2019-08-13 02:31:39 +0300

    overlay menu fix

--- Overlay.js
+++ Overlay.js
@@ -7,6 +7,9 @@ import ProductInfo from './ProductInfo'
 import useComponentSize from '@rehooks/component-size'
 import './Overlay.css'
 import saladsData from './saladsData';
+import { useKeyPress } from 'react-use';
+import { classList } from './util'
+
 
 
 export default function Overlay ({ setOverlayVisible, activeProduct, setActiveProduct  }) {
@@ -21,18 +24,45 @@ export default function Overlay ({ setOverlayVisible, activeProduct, setActivePr
         salads:  saladsData,
     }
 
-    const productLabels = { pizzas: 'ПИЦЦЫ', noodles: 'ЛАПША', salads: 'САЛАТЫ' }
+    const productLabels = { pizzas: 'Пицца', noodles: 'Лапша', salads: 'Салаты' }
 
     return <div className='product-overlay'>
                 <div className='overlay-menu'>
-                    <div className='back' onClick={() => setOverlayVisible (false)} >⇐</div>
+                    <div className='back' onClick={() => setOverlayVisible (false)} >⇐ Назад</div>
                     
-                    {dropdownMenuVisible
+                    {/* {dropdownMenuVisible
                         ? <div className='choose-type' onClick={() => dropdownMenuVisible ? setDropdownMenuVisible(false) : setDropdownMenuVisible(true)}>
                               {Object.entries(productLabels).map(([k, v]) => <div onClick={() => setActiveProduct(k)}>{v}</div>)}
                           </div>
-                        : <div className='' onClick={() => setDropdownMenuVisible(true)}>{productLabels[activeProduct]}</div>}
-                    <div className='choose-taste'>Выбрать тип начинки</div>
+                        : <div className='' onClick={() => setDropdownMenuVisible(true)}>{productLabels[activeProduct]}</div>} */}
+
+                    {/* {dropdownMenuVisible
+                        ? <div className='choose-type' onClick={() => dropdownMenuVisible ? setDropdownMenuVisible(false) : setDropdownMenuVisible(true)}>
+                              {Object.entries(productLabels).map(([k, v]) => <div onClick={() => setActiveProduct(k)}>{v}</div>)}
+                          </div>
+                        : <div className='' onClick={() => setDropdownMenuVisible(true)}>{productLabels[activeProduct]}</div>} */}
+                    
+                    {/*<div className={classList ({ 'size-switch': 1, 'active': isLarge, 'clicked': isClicked })} onClick={onClick}></div> */}   
+
+                    {dropdownMenuVisible
+                        ? <div className={classList ({'choose-type': 1, 'active': dropdownMenuVisible})} onClick={() => setDropdownMenuVisible(!dropdownMenuVisible) }>
+                              <ul>
+                                  <li>{productLabels[activeProduct]}</li>
+                                  {
+                                      Object.entries(productLabels).map(([k, v]) =>
+                                            v !== productLabels[activeProduct]
+                                                ? <li onClick={() => setActiveProduct(k)}>{v}</li>
+                                                : undefined)
+                                  }
+                              </ul>
+                          </div>
+                        : <li className='' onClick={() => setDropdownMenuVisible(true)}>{productLabels[activeProduct]}</li>
+                    }
+
+
+
+
+                    <div className='choose-taste'>Выбрать начинку</div>
                     <div className='bucket'>Корзина</div>
                 </div>
                 <div className='product-choose-panel'>
