18d43dea94e6001f78f0e6f76c4553ebbf9186fa
inversia <mrs.shpritz@gmail.com>
2019-09-03 11:20:15 +0300

    placeholder fix

--- CartItem.js
+++ CartItem.js
@@ -9,9 +9,9 @@ export default function CartItem ({ item }) {
 
     return <>
         <div className='orders-list'>   
-            {<SizeSwitch className ='switch' isLarge={item.isLarge} setIsLarge={ isLarge => setCartItemSize (item, isLarge) } /> }
             <div className='item-image' style={{backgroundImage: item.backgroundImage}}></div>
             <span>{item.name}</span>
+            {<SizeSwitch className ='switch' isLarge={item.isLarge} setIsLarge={ isLarge => setCartItemSize (item, isLarge) } /> }
             <div onClick={() => removeFromCart(item)}className='trash'></div>
             {/* <div className='price'>{item.price}</div> */}
             
