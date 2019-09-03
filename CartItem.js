393245b27ab6613c0ca3192617323d37b91b34e4
inversia <mrs.shpritz@gmail.com>
2019-09-03 10:16:18 +0300

    rocket animation add

--- CartItem.js
+++ CartItem.js
@@ -8,11 +8,13 @@ export default function CartItem ({ item }) {
     const { removeFromCart, setCartItemSize } = useContext (CartContext)
 
     return <>
-        <div className='orders-list'>
-            <SizeSwitch className ='switch' isLarge={item.isLarge} setIsLarge={ isLarge => setCartItemSize (item, isLarge) } />    
+        <div className='orders-list'>   
+            {<SizeSwitch className ='switch' isLarge={item.isLarge} setIsLarge={ isLarge => setCartItemSize (item, isLarge) } /> }
             <div className='item-image' style={{backgroundImage: item.backgroundImage}}></div>
             <span>{item.name}</span>
             <div onClick={() => removeFromCart(item)}className='trash'></div>
+            {/* <div className='price'>{item.price}</div> */}
+            
         </div>
     </>
 }
