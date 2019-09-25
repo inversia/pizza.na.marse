e55127342c4db7175b8cf13707ee0b6870958255
inversia <mrs.shpritz@gmail.com>
2019-09-25 05:29:56 +0300

    cart fix and refactoring

--- Cart.js
+++ Cart.js
@@ -3,13 +3,17 @@ import './Cart.css'
 import CartItem from './CartItem'
 import { CartContext } from './CartContext'
 import { classList } from './util'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 export default function Cart () {
 
     const form = useRef ()
 
-    function submitFormHandler (event) {
+    const [submitClicked, setSubmitClicked] = useState (false)
+
+    function onSubmit (event) {
         
+        setSubmitClicked (true)
         event.preventDefault()
 
         //alert(form.current)
@@ -18,28 +22,37 @@ export default function Cart () {
     const { cartItems, addRandomPizza, removeFromCart, setIsLarge } = useContext (CartContext)
     const isCartEmpty = cartItems.length === 0
 
-    const [submitClicked, setSubmitClicked] = useState (false)
-
     return  <div className='cart-content'>
                 {isCartEmpty ? 
                     <h1>К сожалению, корзина пока пуста :(</h1> 
                     : <h1 className='target'>Итак, мы уже почти у цели!</h1>
                 }
-                <h2>Напоминаем, что мы доставляем только в радиусе метро Курская
-                    <div className='rocket'></div>
-                </h2>
                 {/* <h3>{pizzaData[randomPizza]}</h3> */}
-                <div className='orders'>
-                    {cartItems.map(item => <CartItem key={item.uid} item={item} />)}
-                </div>
-                <div className='random'>Если Вас одолевают муки выбора, можете попытать удачи и <br/><span onClick={() => addRandomPizza()}>заказать рандомную пиццу</span></div>
-                {/* <pre>{JSON.stringify (cartItems, null, 4)}</pre> */}
-                <form onSubmit={submitFormHandler} ref={form} className={classList ({ fields: 1, 'submit-clicked': submitClicked })}>                        
-                    <input type='text' name='name'   placeholder='Как к Вам обращаться?'/>
-                    <input type='text' name='address' placeholder='Адрес доставки (напоминаем, что мы доставляем только в радиусе метро Курская)' required />
-                    <input type='tel' pattern='^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$' name='phone'  placeholder='Номер для связи' required />
-                    <input type='text' name='notes'  placeholder='Пометки'/>
-                    <input type='submit' value='Оформить заказ' onClick={() => setSubmitClicked (true)} />
-                </form>  
+
+                <div className='inner'>
+                    <div className='delivery-radius'>Напоминаем, что мы доставляем только в радиусе метро Курская
+                        <div className='rocket'></div>
+                    </div>
+
+                    <div class={classList ({ 'cart-items': 1, 'empty': !cartItems.length })}>
+                        <TransitionGroup component={null}>
+                            {cartItems.map (item =>
+                                <CSSTransition key={item.uid} timeout={500} classNames='cart-item'>
+                                   <CartItem item={item} />
+                                </CSSTransition>
+                            )}
+                        </TransitionGroup>
+                    </div>
+                
+                    <div className='random'>Если Вас одолевают муки выбора, можете попытать удачи и <br/><span onClick={() => addRandomPizza()}>добавить рандомную пиццу</span></div>
+                    {/* <pre>{JSON.stringify (cartItems, null, 4)}</pre> */}
+                    <form ref={form} className={classList ({ fields: 1, 'submit-clicked': submitClicked })}>                        
+                        <input type='text' name='name'   placeholder='Как к Вам обращаться?'/>
+                        <input type='text' name='address' placeholder='Адрес доставки (напоминаем, что мы доставляем только в радиусе метро Курская)' required />
+                        <input type='tel' pattern='^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$' name='phone'  placeholder='Номер для связи' required />
+                        <input type='text' name='notes'  placeholder='Пометки'/>
+                    </form>
+                    <button class='submit' onClick={onSubmit}>Оформить заказ</button>
+                </div> 
             </div>
 }
