0a3c18bd979b47f0b9919f8549711df4fafc2ba2
inversia <mrs.shpritz@gmail.com>
2019-10-20 01:38:25 +0300

    cart mobile version wip

--- MenuMobile.js
+++ MenuMobile.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import { A } from 'hookrouter'
-import CartCounter from './CartCounter';
-import { classList } from './util'
+import React, { useContext } from 'react'
+import { A , navigate} from 'hookrouter'
+import CartCounter     from './CartCounter';
+import { CartContext } from './CartContext'
+import { classList }   from './util'
 
-const MenuMobile = ({onSelect }) =>
-    <div className="menu mobile">
+export default function MenuMobile ({onSelect }) {
+
+    const { addRandomPizza } = useContext (CartContext)
+
+    return <div className="menu mobile">
         <a href="javascript:{}" className="hamburger-menu link"></a>
         <a href="javascript:{}" className="meat link" onClick={() => onSelect ('meat') }></a>
         <a href="javascript:{}" className="broccoli link" onClick={() => onSelect ('veg') }></a> 
         <a href="javascript:{}" className="fish link" onClick={() => onSelect ('fish') }></a> 
-        <a href="javascript:{}" className="mix link" onClick={() => onSelect ('mix') }></a> 
-        {/* <A {...props ('cart')}><CartCounter /></A>  */}
+        <a href="javascript:{}" className="mix link" onClick={() => { addRandomPizza (); navigate ('/cart') }}></a> 
         <A href="/cart" className="cart link"><CartCounter /></A> 
     </div>
-
-export default MenuMobile
+}
