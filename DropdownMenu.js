3f4a90557d3a87caa4698d984312c6515d210c52
inversia <mrs.shpritz@gmail.com>
2019-11-21 04:41:10 +0300

    useClickAway add

--- DropdownMenu.js
+++ DropdownMenu.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { classList } from './util'
 import './DropdownMenu.css'
+import {useClickAway} from 'react-use';
 const { entries } = Object
+
 
 export default function DropDownMenu ({ className, items = {}, defaultText, enableSelectAll = true, activeItem, setActiveItem }) {
 
     //const [visible, setVisible] = useState (!defaultText ? true : false)
     const [visible, setVisible] = useState (false)
 
+    const dropdownMenu = useRef(null)
+
+    useClickAway(dropdownMenu, () => {
+        setVisible (false)
+    })
+
     return <>
          
-        {<div className={classList({ 'dropdown-menu': 1, [className]: 1, active: visible })} onClick={() => setVisible (!visible)} >
+        {<div ref={dropdownMenu} className={classList({ 'dropdown-menu': 1, [className]: 1, active: visible })} onClick={() => setVisible (!visible)} >
             {activeItem ? items[activeItem] : defaultText}
             <ul>
                 {[
