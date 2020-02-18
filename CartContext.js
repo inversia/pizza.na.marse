6d773600d44be4ea474dca25b664af4e9d04dc73
inversia <mrs.shpritz@gmail.com>
2020-02-19 02:52:20 +0300

    localStorage random pizza fix

--- CartContext.js
+++ CartContext.js
@@ -9,21 +9,21 @@ function useCartState () {
 
     const [cartItems, setCartItems] = useState (JSON.parse(localStorage.getItem('cartItems')))
 
-    const addToCart        = item            => setCartItems ([...cartItems, Object.assign (item, { uid: genCartItemUid () }) ])
-                                                
+    const addToCart        = item            => setCartItems ([...cartItems, Object.assign (item, { uid: genCartItemUid () }) ])   
                                                 localStorage.setItem('cartItems', JSON.stringify(cartItems))
-                                                console.log(localStorage)
 
     const removeFromCart   = item            => setCartItems (cartItems.filter (otherItem => otherItem !== item))
+
     const setCartItemSize  = (item, isLarge) => {
-                                                    item.isLarge = isLarge
-                                                    setCartItems ([...cartItems])
+                                                item.isLarge = isLarge
+                                                setCartItems ([...cartItems])
                                                 }
     const addRandomPizza = () => {
 
         const randomPizza = Math.floor(Math.random() * pizzaData.length)
 
         addToCart({ productType: 'pizzas', ...pizzaData[randomPizza], isLarge: true }) 
+        localStorage.setItem('cartItems', JSON.stringify({ productType: 'pizzas', ...pizzaData[randomPizza], isLarge: true }))
     }
 
     return { cartItems, addToCart, removeFromCart, setCartItemSize, addRandomPizza }
