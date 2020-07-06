ad587b0f23ac61c6b7f166a010d29faba697d8dd
inversia <mrs.shpritz@gmail.com>
2020-07-07 00:42:27 +0300

    fix

--- publish.sh
+++ publish.sh
@@ -1,8 +1,10 @@
 echo Publishing to GitHub Pages \
 && git checkout gh-pages \
 && cp .gitignore .gitignore_saved \
+&& cp CNAME CNAME_saved
 && git rm -r '*' \
 && mv .gitignore_saved .gitignore \
+&& mv CNAME_saved CNAME \
 && cp -rf dist/* . \
 && git add --ignore-errors -A \
 && git commit -am "new build" \
